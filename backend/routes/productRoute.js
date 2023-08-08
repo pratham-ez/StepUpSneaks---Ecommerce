4f7ed4614963b53121d3764b146bb14cf2b72175
Pratham Khodwe <prathamkhodwe@gmail.com>
2023-08-08 16:05:05 +0530

    Completed all User and Product API

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { getAllProducts, createProduct, updateProduct, deleteProduct, getProductDetails } = require("../controllers/productController");
+const { getAllProducts, createProduct, updateProduct, deleteProduct, getProductDetails, createProductReview, getProductReviews, deleteReview } = require("../controllers/productController");
 const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
 const router = express.Router();
 
@@ -9,3 +9,10 @@ router.route("/admin/product/:id").put(isAuthenticatedUser, authorizeRoles("admi
 module.exports = router;
 
 router.route("/product/:id").get(getProductDetails)
+
+router.route("/review").put(isAuthenticatedUser, createProductReview)
+
+router.route("/reviews").get(getProductReviews).delete(isAuthenticatedUser, deleteReview)
+
+
+
