021b87415b184824a5a4b8f88823520e10ba99e1
Pratham Khodwe <prathamkhodwe@gmail.com>
2023-08-09 11:34:42 +0530

    Started frontend and installed and initialized all packages and react app

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+import Header from "./component/layout/Header.jsx"
+import { BrowserRouter as Router } from 'react-router-dom';
+
+
+function App() {
+  return (
+    <Router>
+      <Header />
+    </Router>
+  );
+}
+
+export default App;
---
+++ frontend/src/component/layout/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { ReactNavbar } from "overlay-navbar"
+const Header = () => {
+    return (
+        <ReactNavbar />
+    )
+}
+
+export default Header
